4c17f4627c74b402b58c51125cf60aebe4e87d1f
Jaspreet Singh Bhatti <ijaspreetbhatti@outlook.com>
2022-03-02 14:28:27 -0800

    Updates Profile Controller with flat profile model

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -11,11 +11,9 @@ const getProfile = async (req, res) => {
 const postProfile = async (req, res) => {
     console.log(req.body)
     const newUser = new profile({
-        credentials: {
-            nickName: req.body.nickName,
-            email: req.body.email,
-            password: req.body.password,
-        }
+        nickName: req.body.nickName,
+        email: req.body.email,
+        password: req.body.password,
     });
     const response = await newUser.save();
     res.json(response);
@@ -24,11 +22,9 @@ const postProfile = async (req, res) => {
 const putProfile = async (req, res) => {
     console.log(req.body)
     const response = await profile.updateOne({ _id: req.params.id }, {
-        credentials: {
-            nickName: req.body.nickName,
-            email: req.body.email,
-            password: req.body.password,
-        }
+        nickName: req.body.nickName,
+        email: req.body.email,
+        password: req.body.password,
     })
     res.json(response);
 };
