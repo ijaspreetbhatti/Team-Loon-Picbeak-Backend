caecd027f1c36bd2ebe55ca9a1e030c3c77fe9e0
Jaspreet Singh Bhatti <ijaspreetbhatti@outlook.com>
2022-03-09 19:56:49 -0800

    Adds Allows cross origin

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 let path = require('path');
 const connected = require("./connection.js");
@@ -15,10 +16,17 @@ connected
 
 const routes = require('./routes/index');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
