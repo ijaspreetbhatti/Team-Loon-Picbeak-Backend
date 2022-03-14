a447f8eeca50008f907c724236d6ab5b883d68ca
Gavriil84 <gabwt.84@gmail.com>
2022-03-14 16:38:46 -0700

    change status

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,7 +6,7 @@ const matchUser = async (req, res) => {
     const user = await profile.findOne({ email, password }).lean();
 
     if (user && user.isActive) {
-        res.json({ status: 'ok', user: user })
+        res.json({ user: user.email })
     } else {
         res.json({ status: 'error', message: 'invalid email or password' })
     }
