c7fd76307b5f70b53f2f355ffd2301e9a791ff53
Jaspreet Singh Bhatti <ijaspreetbhatti@outlook.com>
2022-03-28 16:24:35 -0700

    Added higher payload size

--- app.js
+++ app.js
@@ -17,9 +17,8 @@ connected
 const routes = require('./routes/index');
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
