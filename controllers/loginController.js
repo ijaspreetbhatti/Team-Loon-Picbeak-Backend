7419c2ef7510c4331d5dd6a6c6db59709558594d
Gavriil84 <gabwt.84@gmail.com>
2022-03-28 14:41:27 -0700

    add info for user

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,7 +6,7 @@ const matchUser = async (req, res) => {
     const user = await profile.findOne({ email, password }).lean();
 
     if (user && user.isActive) {
-        res.json({ user: user.email })
+        res.json({ user: user.email, name: user.nickName })
     } else {
         res.json({ status: 'error', message: 'invalid email or password' })
     }
